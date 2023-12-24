7f71f67f6e360b01713a2cea06c1e8ef7adcdf2f
Ari <DonSagiv@users.noreply.github.com>
2023-12-24 15:55:13 -0500

    Re-implement Opensilver.

---
+++ asagiv.UI.gptAssistant.OpenSilver/asagiv.UI.gptAssistant.OpenSilver.Browser/Pages/Index.cs
@@ -0,0 +1,26 @@
+﻿using asagiv.UI.gptAssistant.OpenSilver.Browser.Interop;
+using DotNetForHtml5;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using Microsoft.JSInterop;
+
+namespace asagiv.UI.gptAssistant.OpenSilver.Browser.Pages
+{
+    [Route("/")]
+    public class Index : ComponentBase
+    {
+        protected override void BuildRenderTree(RenderTreeBuilder __builder)
+        {
+        }
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            Cshtml5Initializer.Initialize(new UnmarshalledJavaScriptExecutionHandler(JSRuntime));
+            Program.RunApplication();
+        }
+
+        [Inject]
+        private IJSRuntime JSRuntime { get; set; }
+    }
+}
