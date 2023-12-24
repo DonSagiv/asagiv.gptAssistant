03ece25672e9f0d32814b560caf7a71d49e5465c
Ari <DonSagiv@users.noreply.github.com>
2023-12-24 09:24:05 -0500

    Remove blazor, apply OpenSilver.

---
+++ src/asagiv.gptAssistant.UI/asagiv.UI.gptAssistant.OpenSilver/asagiv.UI.gptAssistant.OpenSilver/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+
+namespace asagiv.UI.gptAssistant.OpenSilver
+{
+    public partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            // Enter construction logic here...
+        }
+    }
+}
